576ab4e9eb364dae321df245888f0cf5320ea54c
OussamaZaitoime7 <developer.Zaitoune@gmail.com>
2024-01-07 21:09:46 +0100

    1.0

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// function openfunc(){
+    
+//     if (document.getElementById("btnfunc").style.display = "none") {
+//         document.getElementById("btnfunc").style.display = "block";
+//     }
+//     if (document.getElementById("btnfunc").style.display = "block"){
+//         document.getElementById("btnfunc").style.display = "none"
+//     }
+
+// }
